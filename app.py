bcf1ec0647b88b3f8c16103e9d2303b230c1e70b
Jarag <JARAGST@pella.com>
2024-08-02 17:10:34 -0400

    refine code with hours and minute calculation

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask,render_template,request
+from datetime import datetime, timedelta
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -22,11 +24,25 @@ def meal():
 @app.route("/alarm",methods = ["GET","POST"])
 def alarm():
     if request.method == "POST":
-        current_time = int(request.form["current_time"])
+        current_hours = int(request.form["current_hours"])
+        current_minutes = int(request.form["current_minutes"])
         hours_to_wait = int(request.form["hours_to_wait"])
-        alarm_time = (current_time + hours_to_wait) % 24
-        period = 'AM' if 0<=alarm_time < 12 else 'PM'
-        return render_template("alarm.html",current_time=current_time,hours_to_wait=hours_to_wait,alarm_time=alarm_time,title="Alarm",period=period)
+        minutes_to_wait = int(request.form["minutes_to_wait"])
+    
+       # Create a datetime object with current hours and minutes
+        now = datetime.now().replace(hour = current_hours,minute=current_minutes, second=0, microsecond=0)
+        wait_time = timedelta(hours=hours_to_wait ,minutes=minutes_to_wait)
+
+        # Calculate the alarm time
+        alarm_time = now + wait_time
+    
+        # Format the alarm time in 24-hour format
+        alarm_time_str = alarm_time.strftime("%H : %M")
+
+        
+        return render_template("alarm.html",current_hours=current_hours,current_minutes=current_minutes,hours_to_wait=hours_to_wait,minutes_to_wait= minutes_to_wait,
+                               alarm_time = alarm_time_str,title="Alarm")
+    
     return render_template("alarm.html",alarm_time = None,title="Alarm")
 
 if __name__=="__main__":
